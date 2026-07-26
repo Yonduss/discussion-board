7d5e7c3140bf07b511316f0f52deeb58e7cd1cab
jun <wnsk335533@gmail.com>
2026-07-26 14:30:02 +0900

    Modified CORS for react FE

--- src/main/java/com/ktb/discussionboard/config/WebConfig.java
+++ src/main/java/com/ktb/discussionboard/config/WebConfig.java
@@ -11,10 +11,19 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(
+                        "http://localhost:5173",
                         "http://127.0.0.1:5500",
                         "http://localhost:5500"
                 )
-                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PATCH",
+                        "DELETE",
+                        "OPTIONS"
+                )
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
     }
 }
