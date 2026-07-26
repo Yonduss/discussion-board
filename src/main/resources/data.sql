88ea8a7453fbdc869da71caa8bab3e36fcfbdb51
jun <wnsk335533@gmail.com>
2026-07-26 14:29:05 +0900

    Modified dummy data for concept

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,7 +12,7 @@ INSERT INTO users (
     'test@test.com',
     '$2a$10$Ipk7Xms7kvXN/TgqqO0b7OJNHhbmCrRBSheHDd7CQGC2HlnrzdWJ.', --12345678
     'test',
-    'https://i.namu.wiki/i/1jdhfuJxvI-RtKFumZD0wggFTdQDOANKk8HVx0Gn57jhUhb-sLhxfUG7cx978fOizT0Lfk6tl15KjbqRKJzoyHZcFsUIT2LXpn0PtnuzHbN38iLVT_ImI2hDQeNf-2BflYeDv7ri2E-YvAZYUdIWSA.svg',
+    'https://i.namu.wiki/i/O88iZiOsgmeag6nqOlfUC36JpLvV4utcUEwk03sqWOmJG6cut9O9WzychNVyqlSfcUDByx42tM61YyUhITzrnjBmQiqfSluC7R61rLEOv1RJUyE_ATaaNbQu_DgyZV9i58B-P2tEhhldpY1tjK8n0Q.svg',
     false,
     CURRENT_TIMESTAMP,
     CURRENT_TIMESTAMP,
@@ -34,7 +34,7 @@ INSERT INTO users (
      'dummy@dummy.com',
      '$2a$10$XW3uqYz62k1H5ux6n5bdk.317aawij1WtgcZy1b4zxpTYjyx7UZk.', --87654321
      'dummy',
-     'https://i.namu.wiki/i/0u4CFwTcXGIiHYDFAtKQ_NJZhx7SlcfYpnNWLOOmyVlsxihHNL8kTM1z6DVRy4SSliMrmjBZo0TxVF1tnrYYgVleQFWohaMINce2MQkQe_XsVJoRnMze5suwKt5STaApqs8xrQzshZVHItMqvwdWRg.svg',
+     'https://i.namu.wiki/i/Jy-CzUrta6Ok4O-gEx3a8VaeybluRWDNl8KiovmnU_YwQIaylqa9yXdbCR67dsvLRtJjtGsTCaDWwcdOAMcf6w.svg',
      false,
      CURRENT_TIMESTAMP,
      CURRENT_TIMESTAMP,
@@ -58,8 +58,12 @@ INSERT INTO posts (
     hidden_at
 ) VALUES (
     1,
-    'Norway v England | Quarter-final',
-    'There was a match between Norway & England played at Miami Stadium on Sunday 12, July at 06:00 KST.',
+    'Latest Dodgers injuries & transactions',
+    'LATEST INJURIES' ||
+    'TWP Shohei Ohtani' ||
+    'Injury: Left knee inflammation' ||
+    'Expected return (as pitcher): TBD, but in 2026' ||
+    'Status: Has continued to play catch and could throw off the mound by the end of the week of July 20, though his timeline to return to the rotation remains undetermined. (updated July 20)',
     0,
     0,
     0,
@@ -88,8 +92,9 @@ INSERT INTO posts (
     hidden_at
 ) VALUES (
      2,
-     'Argentina v Switzerland | Quarter-final',
-     'There was a match between Argentina & Switzerland played at Kansas Stadium on Sunday 12, July at 10:00 KST.',
+     'With trade deadline looming, Texas Rangers need wins, health',
+     'President of baseball operations Chris Young still believes the Texas Rangers can get better before the trade deadline, but they need to play winning baseball and get Corey Seager healthy.' ||
+     'ARLINGTON — Even after dropping 2 of 3 weekend games in Atlanta, the Texas Rangers entered play Monday still atop the American League West. It’s still mind-boggling that the Rangers are playoff contenders, but it’s true.',
      0,
      0,
      0,
@@ -116,7 +121,7 @@ INSERT INTO comments (
     1,
     2,
     NULL,
-    'Because maybe you''re gonna be the one that saves me.',
+    'No way! I can''t believe it.',
     false,
     false,
     CURRENT_TIMESTAMP,
@@ -138,7 +143,7 @@ INSERT INTO comments (
     1,
     1,
     1,
-    'And after all, you''re my wonderwall.',
+    'Not sure he has a serious injury.',
     false,
     false,
     CURRENT_TIMESTAMP,
@@ -160,7 +165,7 @@ INSERT INTO comments (
     2,
     1,
     NULL,
-    'VAMOS!!! MESSI!!!',
+    'Rangers!!',
     false,
     false,
     CURRENT_TIMESTAMP,
@@ -174,7 +179,7 @@ INSERT INTO post_images (
     sort_order
 ) VALUES (
     1,
-    'https://cdn-media.theathletic.com/cdn-cgi/image/width=1440%2cquality=70%2cformat=auto/https://cdn-media.theathletic.com/FV7FjolqeFqF_qlbnRfjVdCBt_1440x960.jpg',
+    'https://s.yimg.com/ny/api/res/1.2/Ax0wlX0COtQTx97iIDO79g--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTU0MDtjZj13ZWJw/https://media.zenfs.com/en/heavy_214/46d74ead5984142fc6fba1f4af26d4af',
     0
 );
 
@@ -184,6 +189,6 @@ INSERT INTO post_images (
     sort_order
 ) VALUES (
     2,
-    'https://cdn-media.theathletic.com/cdn-cgi/image/width=1440%2cquality=70%2cformat=auto/https://cdn-media.theathletic.com/2tI5kiV1ItAO_RCOEDgnyIS25_1440x960.jpg',
+    'https://cdn.allcitynetwork.com/wp-content/uploads/sites/14/2026/07/20183953/Seager7-20-1024x683.jpg',
     0
 );
